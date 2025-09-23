d19ff7e439f37abe443525821d3513c10acf4379
abazhaki <158191545+abazhaki@users.noreply.github.com>
2025-09-23 23:47:37 +0800

    Update recruitment_ui.py

--- recruitment_ui.py
+++ recruitment_ui.py
@@ -33,9 +33,9 @@ def read_encrypted_excel(file_path, password):
     return df
     
 # 指定Excel文件路径 - 请在此处修改为你的Excel文件实际路径
-EXCEL_FILE_PATH = "results.xlsx"  # 例如: "C:/data/students.xlsx" 或 "./data/results.xlsx"
+EXCEL_FILE_PATH = st.secrets["DB_FILEPATH"]  # 例如: "C:/data/students.xlsx" 或 "./data/results.xlsx"
 EXCEL_PASSWORD = st.secrets["DB_PASSWORD"]
-st.write("DB password:", st.secrets["DB_PASSWORD"])
+
 # 初始化数据框
 df = None
 
